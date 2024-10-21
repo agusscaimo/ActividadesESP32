2eefd86bdeb61e65ee6cdfb86e0dda62e11c0359
agusscaimo <caimoagustinbautista@gmail.com>
2024-10-21 17:23:31 -0300

    Punto2.ino

--- Punto2/Punto2.ino
+++ Punto2/Punto2.ino
@@ -1,6 +1,7 @@
 #include <WiFi.h>
 
 #define PORT 80
+WiFiServer server(PORT);
 
 const char* ssid     = "CESJT";
 const char* password = "itisjtsmg";
@@ -17,6 +18,10 @@ const int segmentos[10][7] = {
   {1, 1, 1, 1, 1, 1, 1},
   {1, 1, 1, 1, 0, 1, 1}
 };
+int e = 0, d = 0, f = 0;
+const int display1 [7] = {13, 12, 14, 27, 26, 25, 33};
+const int display2 [7] = {15, 5, 18, 19, 21, 22, 23};
+
 int contconexion = 0;
 
 String palabra;
@@ -32,19 +37,20 @@ String pagina =
     "<body>"
       "<center>"
         "<h1>Control dos 7 segmentos</h1>"
-        "<p><a href='/animacion1'><button style='height:50px;width:100px'>ASCENDENTE</button></a></p>"
-        "<p><a href='/animacion2'><button style='height:50px;width:100px'>DESCENDENTE</button></a></p>"
-        "<p><a href='/animacion2'><button style='height:50px;width:100px'>PARAR CONTEO</button></a></p>"
-        "<p><a href='/animacion2'><button style='height:50px;width:100px'>REINICIAR CONTEO</button></a></p>"
+        "<p><a href='/ascendente'><button style='height:50px;width:100px'>ASCENDENTE</button></a></p>"
+        "<p><a href='/descendente'><button style='height:50px;width:100px'>DESCENDENTE</button></a></p>"
+        "<p><a href='/parar'><button style='height:50px;width:100px'>PARAR CONTEO</button></a></p>"
+        "<p><a href='/reiniciar'><button style='height:50px;width:100px'>REINICIAR CONTEO</button></a></p>"
       "</center>"
     "</body>"
   "</html>";
 
 
 void setup() {
-  for (int i = 0; i < 5; i++) {
-    pinMode(leds[i], OUTPUT); 
-    digitalWrite(leds[i], LOW);
+  Serial.begin(115200);
+  for (int i = 0; i < 7; i++) {
+    pinMode(display1[i], OUTPUT);
+    pinMode(display2[i], OUTPUT);
   }
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED and contconexion <50) { 
@@ -80,44 +86,49 @@ void loop(){
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
-            if (palabra.indexOf("GET /animacion1") >= 0) {
-              //Oruga ida solo
-              digitalWrite(leds[0], HIGH);
-              digitalWrite(leds[1], LOW);
-              digitalWrite(leds[2], LOW);
-              digitalWrite(leds[3], LOW);
-              digitalWrite(leds[4], LOW);
-              delay(1000);
-              digitalWrite(leds[0], LOW);
-              digitalWrite(leds[1], HIGH);
-              delay(1000);
-              digitalWrite(leds[1], LOW);
-              digitalWrite(leds[2], HIGH);
-              delay(1000);
-              digitalWrite(leds[2], LOW);
-              digitalWrite(leds[3], HIGH);
-              delay(1000);
-              digitalWrite(leds[3], LOW);
-              digitalWrite(leds[4], HIGH);
-              delay(1000);
-            } else if (palabra.indexOf("GET /animacion2") >= 0) {
-              //Se prenden intercaladamente
-              digitalWrite(leds[0], HIGH);
-              digitalWrite(leds[1], LOW);
-              digitalWrite(leds[2], HIGH);
-              digitalWrite(leds[3], LOW);
-              digitalWrite(leds[4], HIGH);
-              delay(1000);
-              digitalWrite(leds[0], LOW);
-              digitalWrite(leds[1], HIGH);
-              digitalWrite(leds[2], LOW);
-              digitalWrite(leds[3], HIGH);
-              digitalWrite(leds[4], LOW);
-            }else if (palabra.indexOf("GET /apagar") >= 0) {
-              for (int e = 0; e < 5; e++) {
-                digitalWrite(leds[e], LOW);
+            if (palabra.indexOf("GET /ascendente") >= 0) {
+              for (e; e < 10;e++) {
+                for (d; d < 10;d++) {
+                  for (f = 0; f < 7;f++) {
+                    digitalWrite(display1[f], segmentos[d][f]);
+                    digitalWrite(display1[f], segmentos[e][f]);
+                    delay(1000);
+                  }
+                  if (d == 9) {
+                    d = 0;
+                  }
+                  if (e == 9) {
+                    e = 0;
+                  }
+                }
               }
-            }  
+            } else if (palabra.indexOf("GET /descendente") >= 0) {
+              for (e; e > 0;e++) {
+                for (d; d > 10;d++) {
+                  for (f = 0; f < 7;f++) {
+                    digitalWrite(display1[f], segmentos[d][f]);
+                    digitalWrite(display1[f], segmentos[e][f]);
+                    delay(1000);
+                  }
+                  if (d == 0) {
+                    d = 9;
+                  }
+                  if (e == 0) {
+                    e = 9;
+                  }
+                }
+              }
+            }else if (palabra.indexOf("GET /parar") >= 0) {
+              for (f = 0; f > 7;f++) {
+                digitalWrite(display1[f], segmentos[d][f]);
+                digitalWrite(display1[f], segmentos[e][f]);
+              }
+            }else if (palabra.indexOf("GET /reiniciar") >= 0) {
+              for (f = 0; f > 7;f++) {
+                digitalWrite(display1[f], segmentos[0][f]);
+                digitalWrite(display1[f], segmentos[0][f]);
+              }
+            }
             client.println(pagina);
             client.println();
             break;
